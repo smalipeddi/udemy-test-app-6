bc9c2b4752d910fa081d56e628e79dac74292f72
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-12-27 14:02:43 -0500

    add migration files

--- db/migrate/20211227184700_create_articles.rb
+++ db/migrate/20211227184700_create_articles.rb
@@ -2,9 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :string
-      t.string :description
-      t.string :text
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_045130) do
+ActiveRecord::Schema.define(version: 2021_12_27_184700) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "description"
-    t.string "text"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
