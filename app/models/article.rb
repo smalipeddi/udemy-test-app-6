507a21f505a2d1c2a817517d73cb5a7994d7ae2c
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2021-12-27 14:51:03 -0500

    add validation to the row data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title , presence: true , length: { minimun: 6 , maximum: 100 }
+    validated: description,, presence: true, length: { minimun: 10 , maximum: 300 }
 end
