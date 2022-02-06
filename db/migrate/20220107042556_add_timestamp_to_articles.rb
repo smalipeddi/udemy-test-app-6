cbf80505fe7ea0102334cfeada759593ab047b5b
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2022-02-06 01:16:09 -0500

    create articles table and add timestamp columns

---
+++ db/migrate/20220107042556_add_timestamp_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimestampToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :create_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
+
